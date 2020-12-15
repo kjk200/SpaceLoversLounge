d63756687d1867125c521e49dcb27f492a31400a
Kiranjit Kaur <11205226+kjkaur82@users.noreply.github.com>
2020-12-15 12:53:06 -0500

    Add files via upload

---
+++ build/precache-manifest.d36d79166e1b4de24b6ea42ae4426eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e88c576566587d3ec9ebf44b268c9ba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a360f3e2fa82f30be39",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "1a360f3e2fa82f30be39",
+    "url": "/static/js/2.894ef9c7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.894ef9c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5baada8851629c7b380",
+    "url": "/static/js/main.e136a1d0.chunk.js"
+  },
+  {
+    "revision": "eb7f6bd42c3f53862fcc",
+    "url": "/static/js/runtime-main.f0bfd6cb.js"
+  }
+]);
